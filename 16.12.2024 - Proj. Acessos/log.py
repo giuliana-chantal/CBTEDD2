6d7e1098311c1aec5a6a202efb5391a90ea58ee8
Giuliana Ferreira Chantal <103539919+giuliana-chantal@users.noreply.github.com>
2024-12-16 14:54:29 -0300

    Delete 16.12.2024 - Proj. Acessos/log.py

--- 16.12.2024 - Proj. Acessos/log.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-class Log:
-    def __init__(self, usuario, tipoAcesso):
-        self.dtAcesso = datetime.now()
-        self.usuario = usuario
-        self.tipoAcesso = tipoAcesso
